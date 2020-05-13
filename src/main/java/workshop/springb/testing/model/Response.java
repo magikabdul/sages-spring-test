6dce6bc1c75abe3c65f6c8bae63c6e91bf992832
Lukasz <my.email>
2020-05-13 16:52:37 +0200

    Rozwiązanie

---
+++ src/main/java/workshop/springb/testing/model/Response.java
@@ -0,0 +1,50 @@
+package workshop.springb.testing.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Response {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String greeting;
+    private LocalDateTime localDateTime;
+
+    public Response() {
+    }
+
+    public Response(String greeting, LocalDateTime localDateTime) {
+        this.greeting = greeting;
+        this.localDateTime = localDateTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+}
---
+++ src/test/java/workshop/springb/testing/service/GreetServiceTest.java
@@ -0,0 +1,91 @@
+package workshop.springb.testing.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import workshop.springb.testing.model.Response;
+import workshop.springb.testing.repository.ResponseRepository;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.when;
+
+/*
+    TODO 2 README
+    Poniższa adnotacja oznacza test JUnit 5 (bez infrastruktury Spring)
+ */
+@ExtendWith(MockitoExtension.class)
+class GreetServiceTest {
+
+    /*
+        TODO 3 README
+        Testujemy warstwę serwisu - metoda  GreetService#greet
+        Ponieważ metoda GreetService#greet korzysta z ResponseRepository:
+
+        1. mock'ujemy ResponseRepository
+            @Mock
+            private ResponseRepository responseRepository;
+        2. tworzymy obiekt, którego API chcemy testować (GreetService), z zależnością (ResponseRepository)
+             @InjectMocks
+             private GreetService greetService;
+     */
+    @Mock
+    private ResponseRepository responseRepository;
+    @InjectMocks
+    private GreetService greetService;
+
+    @Test
+    @DisplayName("greet(\"X\", true) should return a Response with HELLO, X! greeting")
+    public void greetXTrue_shouldReturn_responseWithHELLOX() throws Exception {
+        String greeting = "Hello, X!";
+        Response stubbedResponse = new Response(greeting, LocalDateTime.now());
+        /*
+            TODO 4 README
+            Ustawiamy zachowanie metody responseRepository.save
+            Jeśli metoda responseRepository.save otrzyma Response z greeting "Hello, X!", niech zwróci obiekt stubbedResponse.
+         */
+        when(responseRepository.save(argThat(response -> greeting.equals(response.getGreeting())))).thenReturn(stubbedResponse);
+
+        /*
+            TODO 5 README
+            Ttestujemy API serwisu (czy metoda GreetService#greet zmieni greeting na pisane wielkimi literami)
+            -metoda GreetService#greet wywoła responseRepository.save,
+            -responseRepository.save zachowa się w sposób zdefiniowany w TODO4
+         */
+        Response returnedFromService = greetService.greet("X", true);
+        assertEquals("HELLO, X!", returnedFromService.getGreeting());
+    }
+
+    @Test
+    @DisplayName("greet(\"X\", false) should return a Response with HI, X! greeting")
+    public void greetXFalse_shouldReturn_responseWithHIX() throws Exception {
+        String greeting = "Hi, X!";
+        Response stubbedResponse = new Response(greeting, LocalDateTime.now());
+
+        when(responseRepository.save(argThat(response -> greeting.equals(response.getGreeting())))).thenReturn(stubbedResponse);
+
+        Response returnedFromService = greetService.greet("X", false);
+        assertEquals("HI, X!", returnedFromService.getGreeting());
+    }
+}
+
+/*
+ _______________________________________________________________________________________________________________________
+ TODO 1 Przeczytaj TODO 2-5, a następnie utwórz analogicznie test:
+ greetXFalse_shouldReturn_responseWithHIX
+ _______________________________________________________________________________________________________________________
+                                                         \
+                                                          \
+                                                            /  \~~~/  \
+                                                           (    ..     )~~~~,
+                                                            \__     __/      \
+                                                              )|  /)         |\
+                                                               | /\  /___\   / ^
+                                                                "-|__|   |__|
+
+ */
